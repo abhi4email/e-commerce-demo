0cc9253927b8636b50f4b24ee18f285d8a84d8e2
Abhi <abhi4email@gmail.com>
2024-04-29 17:43:54 +0530

    updated icon

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 /* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
+https://www.developersiq.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */
+https://www.developersiq.com/Makoto_msk */
 
 export const storeProducts = [
   {
--- src/index.js
+++ src/index.js
@@ -21,7 +21,5 @@ ReactDOM.render(
  </ProductProvider> ,
      document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
